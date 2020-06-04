cc7233ef1033affa879820ab76ae73fa227d8036
denis <denis@localhost.localdomain>
2020-06-04 17:29:45 +0300

    add scripts

---
+++ scripts/createCluster1.sh
@@ -0,0 +1,1 @@
+kind create cluster --image kindest/node:v1.18.2 --config=/configs/config_cluster1.yaml --name kind-1
---
+++ scripts/createCluster2.sh
@@ -0,0 +1,1 @@
+kind create cluster --image kindest/node:v1.18.2 --config=/configs/config_cluster2.yaml --name kind-2
